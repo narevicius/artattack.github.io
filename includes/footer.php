d4ae026d1b7613c279f3bdf27a74746c1b9d59c4
narevicius <jhoow93@gmail.com>
2020-04-24 18:07:32 -0300

    55

--- includes/footer.php
+++ includes/footer.php
@@ -8,7 +8,7 @@
                   
                   <p class="text-center"> <a href="index.html"><img class="hvr-wobble-vertical" style="width: 18rem;" src="img/logo.svg" alt="" title="" /></a> <br> <br> 
 
-Rua Rodovalho Júnior, 475 - Penha de França, São Paulo - SP</p>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.3029048682697!2d-46.55229068447653!3d-23.521605466011632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce642e7d5c1d47%3A0x30376f5fab9c7992!2sArt%20Attack%20Propaganda%20e%20Designer!5e0!3m2!1spt-BR!2sbr!4v1586865257235!5m2!1spt-BR!2sbr" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></p>
                </div>
             </div>
          </div>
